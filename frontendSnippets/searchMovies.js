83a30381edf7ef1a66a79a7c0b8502641b1757d3
YouOldMaid <karen.huaulme@gmail.com>
2021-06-17 09:59:28 -0500

    Create searchMovies.js

---
+++ frontendSnippets/searchMovies.js
@@ -0,0 +1,22 @@
+async function searchMovies() {
+
+      let text = "";
+
+      let searchTerm = search.value;
+      const webhook = "";
+
+      const url = webhook +"?arg=" + searchTerm;
+
+      try{   
+        const movies = await fetch(url)
+        .then(res =>res.json());
+      
+        console.log(movies)
+
+        resultDisplay.innerHTML = text;     
+            
+      } catch (error) {
+      console.log("Whoopsie!", error);
+      }
+
+    }
