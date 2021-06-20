d644b76a524cf8ed23e2e16b896f995d4fe3dc2e
YouOldMaid <karen.huaulme@gmail.com>
2021-06-20 18:50:26 -0500

    Update searchMovieTitles.js

--- frontendSnippets/searchMovieTitles.js
+++ frontendSnippets/searchMovieTitles.js
@@ -1,36 +1,32 @@
-search.addEventListener('input', searchMovieTitles);
-
 async function searchMovieTitles() {
       
-      resultDisplay.innerHTML = '';
-      const searchTerm = search.value;
-      const autocompleteURL ="";
+  resultDisplay.innerHTML = '';
+  autoDisplay.innerHTML='';
 
-      autoDisplay.innerHTML='';
+  const searchTerm = search.value;
+  const autocompleteURL ="";
 
-      if (searchTerm.length < 4){
-        return;
-      } 
 
-      try {
-        const titles = await fetch(autocompleteURL+`?arg=${searchTerm}`)
-          .then(res => res.json());
-      
-        console.log(titles);
+  if (searchTerm.length < 4){
+    return;
+  } 
 
-      } catch (error){
-        console.log("Whoopsie!", error);
-      } 
-      
+  try {
+    const titles = await fetch(autocompleteURL+`?arg=${searchTerm}`)
+      .then(res => res.json());
+  
+    if (titles.length >0){
+
+      titles.map(title =>{
+        let div = document.createElement('div');
+        div.innerHTML=`<h2>${title.title}</h2><hr>`;
+        div.addEventListener("click", ()=>fillMovieTitle(title.title));
+        autoDisplay.appendChild(div);
+      });      
     }
 
-// ***********************
- if (titles.length >0){
-
-    titles.map(title =>{
-      let div = document.createElement('div');
-      div.innerHTML=`<h2>${title.title}</h2><hr>`;
-      div.addEventListener("click", ()=>fillMovieTitle(title.title));
-      autoDisplay.appendChild(div);
-    });      
-  }
+  } catch (error){
+    console.log("Whoopsie!", error);
+  } 
+      
+}
